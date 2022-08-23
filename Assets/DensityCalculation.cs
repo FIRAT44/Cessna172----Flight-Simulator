d4f5bed4a350a2512715b1569a232e84784a702d
jack <tnrfrt01@gmail.com>
2022-08-23 16:31:06 +0300

    Cessna 172
    Cessna 172  flight simulator

---
+++ Assets/DensityCalculation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DensityCalculation : MonoBehaviour
+{
+    public int pressure = 101325;
+    public int temperature = 288;
+
+    public GameObject plane;
+
+}
---
+++ Assets/NewCameraScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCameraScript : MonoBehaviour
+{
+
+    public GameObject plane;
+
+    Vector3 aradakiMesafe;
+
+    private void Start()
+    {
+        aradakiMesafe = gameObject.transform.position - plane.transform.position;
+
+    }
+
+    private void LateUpdate()
+    {
+        gameObject.transform.position = plane.transform.position + aradakiMesafe;
+    }
+}
